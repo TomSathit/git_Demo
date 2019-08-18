f5ece4e4abde7db099c71d8e1f9a8d20c3dc0637
TomSathit <sathit.b5800315@gmail.com>
2019-08-19 01:19:05 +0700

    V1 just try

---
+++ pythonfnlearn.py
@@ -0,0 +1,1 @@
+text = "Today is sunday. I Stay at home and keep coding. Today is Monday. I stay at office and keep cleaning but still keep coding"
---
+++ test.py
@@ -0,0 +1,10 @@
+a = "3"
+b = 5
+
+s="sathit sasikam"
+
+print("Hi" + a + "fuck" + str(b))
+print("Hi %s fuck %d " %(a,b))
+print(s[2])
+print(s[-5])
+print(s[2:5])
